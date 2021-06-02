a1df420d8395f7dd83fb85af4ed5e7d00dcf27e3
richieyoum <seongmin.youm@mail.mcgill.ca>
2021-06-02 02:01:09 -0400

    added inference script

---
+++ scripts/inference.py
@@ -0,0 +1,25 @@
+from util import use_pkl
+import json
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
+
+# load model configs
+with open('../config/train_config.json','r') as f:
+    configs = json.load(f)
+model_config = configs['bilstm']
+
+# laod model
+model = load_model('../models/bilstm')
+
+# load tokenizer
+tokenizer = use_pkl('../models/bilstm/tokenizer.pkl','rb')
+
+# sample sentences to run inference on
+sample_text = ["What kind of TV shows do you like to watch?", "What do you use your phone for?", "Whale is a big animal!", "Do you like video games?"]
+transformed_text = tokenizer.texts_to_sequences(sample_text)
+transformed_text = pad_sequences(sequences=transformed_text, padding='post', truncating='post', maxlen=model_config['max_seq_len'])
+
+pred = model.predict(transformed_text)
+
+for i in zip(sample_text, pred):
+    print(i)
