1a52b5d7fac1c1609c65727ae87d735684689426
richieyoum <seongmin.youm@mail.mcgill.ca>
2021-06-02 14:10:52 -0400

    fixed typo and added more samples

--- scripts/inference.py
+++ scripts/inference.py
@@ -8,14 +8,14 @@ with open('../config/train_config.json','r') as f:
     configs = json.load(f)
 model_config = configs['bilstm']
 
-# laod model
+# load model
 model = load_model('../models/bilstm')
 
 # load tokenizer
 tokenizer = use_pkl('../models/bilstm/tokenizer.pkl','rb')
 
 # sample sentences to run inference on
-sample_text = ["What kind of TV shows do you like to watch?", "What do you use your phone for?", "Whale is a big animal!", "Do you like video games?"]
+sample_text = ["What kind of TV shows do you like to watch?", "What do you use your phone for?", "Do you work from home?", "Whale is a big animal!", "Do you like video games?", "What's your phone number please?"]
 transformed_text = tokenizer.texts_to_sequences(sample_text)
 transformed_text = pad_sequences(sequences=transformed_text, padding='post', truncating='post', maxlen=model_config['max_seq_len'])
 
